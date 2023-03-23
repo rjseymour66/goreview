ff72c7560236adc6b5acfde2f3cbb38760dae2b7
rseymour <rjs.github@gmail.com>
2023-03-22 22:28:17 -0400

    completed tests

--- todo/cmd/todo/todo_test.go
+++ todo/cmd/todo/todo_test.go
@@ -1,9 +1,92 @@
 package main_test
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+// When you test command line tools, you can build the tool using the testing.M type,
+// then use the binary that you built to execute integration tests that you define
+// after the TestMain (m.Run() executes the integration tests). Integration tests accept
+// the testing.T type.
+
 // create vars for the binary and todo file
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
 
 // TestMain(m *testing.M)
-// builds the tool by constructing the build command
-// executes the build command
-// m.Run()???
-// clean up resources
+
+func TestMain(m *testing.M) {
+
+	// builds the tool by constructing the build command
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+	// m.Run tests if main exits correctly
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTaskFromArguments", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add", task)
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	task2 := "test task number 2"
+	t.Run("AddNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := fmt.Sprintf("[ ] 1: %s\n[ ] 2: %s\n", task, task2)
+
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+		}
+	})
+}
