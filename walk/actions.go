af7855e5ebdd71a9e2b9079bbccb436184a92807
rseymour <rjs.github@gmail.com>
2023-04-01 10:34:25 -0400

    completed tests for actions

--- walk/actions.go
+++ walk/actions.go
@@ -8,11 +8,11 @@ import (
 	"path/filepath"
 )
 
-// Filters out the given path if it is a directory, is greater than the minSize,
+// Filters out the given path if it is a directory, is less than the minSize,
 // or the file extension does not match the ext
 func filterOut(path string, ext string, minSize int64, info os.FileInfo) bool {
 	// check if it is a directory
-	if !info.IsDir() || info.Size() > minSize {
+	if info.IsDir() || info.Size() < minSize {
 		return true
 	}
 	// check file extension
--- walk/actions_test.go
+++ walk/actions_test.go
@@ -1,7 +1,65 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestFilterOut(t *testing.T) {
+	type testCase struct {
+		file     string
+		ext      string
+		size     int64
+		expected bool
+	}
 
+	tt := map[string]testCase{
+		"FilterNoExtension": {
+			file:     "testdata",
+			ext:      "",
+			size:     0,
+			expected: true,
+		},
+		"FilterExtensionMatch": {
+			file:     "testdata/dir.log",
+			ext:      ".log",
+			size:     0,
+			expected: false,
+		},
+		"FilterExtensionNoMatch": {
+			file:     "testdata/dir.log",
+			ext:      ".sh",
+			size:     0,
+			expected: true,
+		},
+		"FilterExtensionSizeMatch": {
+			file:     "testdata/dir.log",
+			ext:      ".log",
+			size:     10,
+			expected: false,
+		},
+		"FilterExtensionSizeNoMatch": {
+			file:     "testdata",
+			ext:      "",
+			size:     20,
+			expected: true,
+		},
+	}
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			// act
+			// get file info
+			info, err := os.Stat(tc.file)
+			if err != nil {
+				t.Errorf("cannot get %s FileInfo", tc.file)
+			}
+			// got := filterOut(path string, ext string, minSize int64, info os.FileInfo) bool
+			got := filterOut(tc.file, tc.ext, tc.size, info)
+
+			// assert
+			if got != tc.expected {
+				t.Errorf("got:  %t\nwant:  %t", got, tc.expected)
+			}
+		})
+	}
 }
